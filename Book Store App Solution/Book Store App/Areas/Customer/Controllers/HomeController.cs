2721191ad65a38c454fbe5a6c64a557b8dbecb18
Swapi-Khande <swapnilkhande6996@gmail.com>
2025-01-08 14:58:38 +0530

    Home controller : Book details view added

--- Book Store App Solution/Book Store App/Areas/Customer/Controllers/HomeController.cs
+++ Book Store App Solution/Book Store App/Areas/Customer/Controllers/HomeController.cs
@@ -1,3 +1,4 @@
+using App.DataAccess.Repository.IRepository;
 using App.Models;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
@@ -9,15 +10,24 @@ namespace Book_Store_App.Areas.Customer.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly IUnitOfWork _unitOfWork;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, IUnitOfWork unitOfWork)
         {
             _logger = logger;
+            _unitOfWork = unitOfWork;
         }
 
         public IActionResult Index()
         {
-            return View();
+            IEnumerable<Product> productList = _unitOfWork.Product.GetAll(includeProps: "Category");
+            return View(productList);
+        }
+
+        public IActionResult Details(int id)
+        {
+            Product product = _unitOfWork.Product.Get(u=>u.Id==id,includeProps: "Category");
+            return View(product);
         }
 
         public IActionResult Privacy()
