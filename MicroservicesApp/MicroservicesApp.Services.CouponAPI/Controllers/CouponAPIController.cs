533c9cde2ea9613e3dddb6a666b907e56997eddc
Swapi-Khande <swapnilkhande6996@gmail.com>
2025-01-15 13:22:45 +0530

    Coupon Controller : GET request added

---
+++ MicroservicesApp/MicroservicesApp.Services.CouponAPI/Controllers/CouponAPIController.cs
@@ -0,0 +1,55 @@
+﻿using MicroservicesApp.Services.CouponAPI.Data;
+using MicroservicesApp.Services.CouponAPI.Models;
+using MicroservicesApp.Services.CouponAPI.Models.Dto;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MicroservicesApp.Services.CouponAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CouponAPIController : ControllerBase
+    {
+        private readonly AppDbContext _db;
+        private ResponseDto _res;
+
+        public CouponAPIController(AppDbContext db)
+        {
+            _db = db;
+            _res = new ResponseDto();
+        }
+
+        [HttpGet]
+        public ResponseDto Get()
+        {
+            try
+            {
+                IEnumerable<Coupon> objList = _db.Coupons.ToList();
+                _res.Result = objList;
+            }
+            catch (Exception ex)
+            {
+                _res.IsSuccess = false;
+                _res.Message = ex.Message;
+
+            }
+            return _res;
+        }
+
+        [HttpGet]
+        [Route("{id:int}")]
+        public ResponseDto Get(int id)
+        {
+            try
+            {
+                Coupon obj = _db.Coupons.First(i=>i.CouponId==id);
+                _res.Result = obj;
+            }
+            catch (Exception ex)
+            {
+                _res.IsSuccess = false;
+                _res.Message = ex.Message;
+            }
+            return _res;
+        }
+    }
+}
--- MicroservicesApp/MicroservicesApp.Services.CouponAPI/obj/Debug/net8.0/MicroservicesApp.Services.CouponAPI.AssemblyInfo.cs
+++ MicroservicesApp/MicroservicesApp.Services.CouponAPI/obj/Debug/net8.0/MicroservicesApp.Services.CouponAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MicroservicesApp.Services.CouponAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2721191ad65a38c454fbe5a6c64a557b8dbecb18")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+063e61527049238eb731f780875d8c7c5ba1636b")]
 [assembly: System.Reflection.AssemblyProductAttribute("MicroservicesApp.Services.CouponAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MicroservicesApp.Services.CouponAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
